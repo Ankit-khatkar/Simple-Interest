ec77d7471e71e6bcc79f9f7814429acf0549cb7f
Itz Ankit Khatkar <72272800+Ankit-khatkar@users.noreply.github.com>
2021-01-27 11:08:13 +0530

    Create Simple Interest.py

---
+++ Simple Interest.py
@@ -0,0 +1,15 @@
+print("Hello, Dcoder!")
+
+print("I am Ankit Khatkar")
+
+print("This program will be help to find simple interest.")
+
+p=int(input("Enter Value of p ;"\n))
+
+q=int(input("Enter value of q ;"\n))
+
+r=int(input("Enter the value of r ;"\n))
+
+Simple interest= p*q*r/2
+
+print("Rs."+ str(Simple interest))
